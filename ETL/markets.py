7f0442ac3ab45acc0d4c10f9e47d42e3db2e061f
Ash-bot19 <Ayushsam3@gmail.com>
2025-10-17 16:35:30 +0530

    Delete ETL directory

--- ETL/markets.py
+++
@@ -1,5 +0,0 @@
-# ISO market codes you care about (keep 10–30 to stay under rate limits easily)
-MARKETS = [
-    "US", "GB", "IN", "DE", "FR", "BR", "JP", "KR", "AU", "CA",
-    "MX", "ES", "IT", "NL", "SE", "ID", "PH", "SG", "AE", "ZA"
-]
